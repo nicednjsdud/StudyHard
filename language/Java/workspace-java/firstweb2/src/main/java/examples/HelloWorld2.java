c6651a546a6dd5becdcfd69213eb80210f0bd98a
nicednjsdud <nicednjsdud@naver.com>
2022-04-29 08:25:59 +0900

    fix
    fix

--- language/Java/workspace-java/firstweb2/src/main/java/examples/HelloWorld2.java
+++ language/Java/workspace-java/firstweb2/src/main/java/examples/HelloWorld2.java
@@ -1,6 +1,8 @@
 package examples;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -26,8 +28,9 @@ public class HelloWorld2 extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("<h1>Hello World</h1>");
 	}
 
 }
